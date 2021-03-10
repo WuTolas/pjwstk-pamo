a71971eb08460a9fc4ce27d455e6c4740ce1d676
WuTolas <wut1337@hotmail.com>
2021-03-10 23:18:33 +0100

    lab-1 validator

--- Lab1/app/src/main/java/com/example/pamo/lab1/MainActivity.java
+++ Lab1/app/src/main/java/com/example/pamo/lab1/MainActivity.java
@@ -18,18 +18,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeComponents();
 
-        bmiOutput = findViewById(R.id.bmiOutput);
-        editWeight = findViewById(R.id.editWeight);
-        editHeight = findViewById(R.id.editHeight);
-        button = findViewById(R.id.button);
-
-        button.setOnClickListener(V -> bmiOutput.setText(calculateBmi().toString()));
+        button.setOnClickListener(V -> {
+            if (isValidated()) {
+                bmiOutput.setText(calculateBmi().toString());
+            }
+        });
     }
 
     private Double calculateBmi() {
         double weight = Double.parseDouble(editWeight.getText().toString());
         double height = Double.parseDouble(editHeight.getText().toString());
         return weight / Math.pow(height, 2);
+    }
+
+    private void initializeComponents() {
+        bmiOutput = findViewById(R.id.bmiOutput);
+        editWeight = findViewById(R.id.editWeight);
+        editHeight = findViewById(R.id.editHeight);
+        button = findViewById(R.id.button);
+    }
+
+    private boolean isValidated() {
+        int errorsCount = 0;
+
+        if (editWeight.getText().toString().isEmpty()) {
+            editWeight.setError(getString(R.string.e_fieldEmpty));
+            errorsCount++;
+        } else if (Double.parseDouble(editWeight.getText().toString()) <= 0) {
+            editWeight.setError(getString(R.string.e_fieldPositive));
+            errorsCount++;
+        }
+
+        if (editHeight.getText().toString().isEmpty()) {
+            editHeight.setError(getString(R.string.e_fieldEmpty));
+            errorsCount++;
+        } else if (Double.parseDouble(editHeight.getText().toString()) <= 0) {
+            editHeight.setError(getString(R.string.e_fieldPositive));
+            errorsCount++;
+        }
+
+        return errorsCount == 0;
     }
 }
