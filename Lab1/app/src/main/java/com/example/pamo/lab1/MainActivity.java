3d2bfce33ae29595c1c4e598f397e8fb07bfd676
WuTolas <wut1337@hotmail.com>
2021-03-11 21:50:48 +0100

    lab-1 refactor

--- Lab1/app/src/main/java/com/example/pamo/lab1/MainActivity.java
+++ Lab1/app/src/main/java/com/example/pamo/lab1/MainActivity.java
@@ -7,29 +7,87 @@ import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Button;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText editWeight;
     private EditText editHeight;
-    private TextView bmiOutput;
-    private Button button;
+    private TextView bmiValueOutput;
+    private TextView bmiTypeOutput;
+    private Button countButton;
+    private Button clearButton;
+
+    private BmiCalculator bmiCalculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeComponents();
 
-        bmiOutput = findViewById(R.id.bmiOutput);
-        editWeight = findViewById(R.id.editWeight);
-        editHeight = findViewById(R.id.editHeight);
-        button = findViewById(R.id.button);
-
-        button.setOnClickListener(V -> bmiOutput.setText(calculateBmi().toString()));
+        clearButton.setOnClickListener(V -> clearValues());
+        countButton.setOnClickListener(V -> prepareCalculatedOutput());
     }
 
-    private Double calculateBmi() {
-        double weight = Double.parseDouble(editWeight.getText().toString());
-        double height = Double.parseDouble(editHeight.getText().toString());
-        return weight / Math.pow(height, 2);
+    private void initializeComponents() {
+        bmiValueOutput = findViewById(R.id.bmiValueOutput);
+        bmiTypeOutput = findViewById(R.id.bmiTypeOutput);
+        editWeight = findViewById(R.id.editWeight);
+        editHeight = findViewById(R.id.editHeight);
+        countButton = findViewById(R.id.countButton);
+        clearButton = findViewById(R.id.clearButton);
+
+        bmiCalculator = new BmiCalculator();
+    }
+
+    private void prepareCalculatedOutput() {
+        if (isValidated()) {
+            bmiCalculator.setWeight(Double.parseDouble(editWeight.getText().toString()));
+            bmiCalculator.setHeight(Double.parseDouble(editHeight.getText().toString()));
+            double bmiValue = bmiCalculator.calculateBmi();
+
+            BmiResultType bmiType = BmiResultType.getBmiResultByValue(bmiValue);
+            bmiValueOutput.setText(String.format(Locale.getDefault(), "%f", bmiValue));
+            bmiTypeOutput.setText(bmiType.getResourceId());
+            bmiTypeOutput.setTextColor(getResources().getColor(bmiType.getColorId(), null));
+        } else {
+            clearResult();
+        }
+    }
+
+    private void clearValues() {
+        editWeight.setText(null);
+        editHeight.setText(null);
+        clearResult();
+
+        editWeight.requestFocus();
+    }
+
+    private void clearResult() {
+        bmiValueOutput.setText(null);
+        bmiTypeOutput.setText(null);
+    }
+
+    private boolean isValidated() {
+        int errorsCount = 0;
+
+        if (editWeight.getText().toString().isEmpty()) {
+            editWeight.setError(getString(R.string.e_fieldEmpty));
+            errorsCount++;
+        } else if (Double.parseDouble(editWeight.getText().toString()) <= 0) {
+            editWeight.setError(getString(R.string.e_fieldPositive));
+            errorsCount++;
+        }
+
+        if (editHeight.getText().toString().isEmpty()) {
+            editHeight.setError(getString(R.string.e_fieldEmpty));
+            errorsCount++;
+        } else if (Double.parseDouble(editHeight.getText().toString()) <= 0) {
+            editHeight.setError(getString(R.string.e_fieldPositive));
+            errorsCount++;
+        }
+
+        return errorsCount == 0;
     }
 }
