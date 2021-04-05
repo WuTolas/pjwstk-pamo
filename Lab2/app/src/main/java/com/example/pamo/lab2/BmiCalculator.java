11110b4df7ed47cf029f70e4623587659b8c8a3f
WuTolas <wut1337@hotmail.com>
2021-04-05 13:41:35 +0200

    LAB-2 init

---
+++ Lab2/app/src/main/java/com/example/pamo/lab2/BmiCalculator.java
@@ -0,0 +1,33 @@
+package com.example.pamo.lab2;
+
+public class BmiCalculator {
+
+    private double weight;
+    private double height;
+    private double result;
+
+    public Double calculateBmi() {
+        result = weight / Math.pow(height, 2);
+        return result;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getResult() {
+        return result;
+    }
+}
---
+++ Lab2/app/src/main/java/com/example/pamo/lab2/BmiResultType.java
@@ -0,0 +1,41 @@
+package com.example.pamo.lab2;
+
+import android.graphics.Color;
+
+import java.util.stream.Stream;
+
+public enum BmiResultType {
+
+    UNDERWEIGHT(R.string.t_underweight, 0.0, 18.5, R.color.teal_700),
+    CORRECT(R.string.t_correctValue, 18.5, 25.0, R.color.green),
+    OVERWEIGHT(R.string.t_overweight, 25.0, 30.0, R.color.orange),
+    OBESITY(R.string.t_obesity, 30.0, 35.0, R.color.red),
+    EXTREME_OBESITY(R.string.t_extremeObesity, 35.0, Double.MAX_VALUE, R.color.purple_700);
+
+    private final int resourceId;
+    private final double minValue;
+    private final double maxValue;
+    private final int colorId;
+
+    BmiResultType(int resourceId, double minValue, double maxValue, int colorId) {
+        this.resourceId = resourceId;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+        this.colorId = colorId;
+    }
+
+    public static BmiResultType getBmiResultByValue(double value) {
+        return Stream.of(BmiResultType.values())
+                .filter(t -> value >= t.minValue && value < t.maxValue)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public int getColorId() {
+        return colorId;
+    }
+}
