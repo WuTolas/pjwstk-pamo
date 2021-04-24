678884c30db9e1beaa5b9a99f42c7e2fcc1e642f
WuTolas <wut1337@hotmail.com>
2021-04-24 20:38:29 +0200

    loading spinner

--- Lab3/app/src/main/java/com/example/pamo/lab3/activity/ChartActivity.java
+++ Lab3/app/src/main/java/com/example/pamo/lab3/activity/ChartActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.example.pamo.lab3.R;
 
@@ -18,6 +19,8 @@ public class ChartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
         WebView chartView = findViewById(R.id.webview_chart);
+        ProgressBar loader = findViewById(R.id.webview_chart_loading);
+        loader.setVisibility(View.VISIBLE);
         chartView.setVisibility(View.GONE);
         chartView.getSettings().setJavaScriptEnabled(true);
         chartView.setWebViewClient(new WebViewClient() {
@@ -26,6 +29,7 @@ public class ChartActivity extends AppCompatActivity {
             {
                 super.onPageFinished(view, url);
                 chartView.loadUrl("javascript:$('body > :not(#graph-cases-daily)').hide();javascript:$('#graph-cases-daily').appendTo('body');");
+                loader.setVisibility(View.GONE);
                 chartView.setVisibility(View.VISIBLE);
             }
         });
